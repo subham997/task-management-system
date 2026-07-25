cb4df80541f2b8720cf2b79637e9b89f0b245609
Subham Samal <subhamsamal@Subhams-MacBook-Pro.local>
2026-07-25 16:57:49 +0530

    feat: generate application models and project structure

---
+++ backend/app/Models/AssignmentLog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AssignmentLog extends Model
+{
+    /** @use HasFactory<\Database\Factories\AssignmentLogFactory> */
+    use HasFactory;
+}
